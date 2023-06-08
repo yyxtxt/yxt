d3b940c12e6f8761fff3463baf505bacb87d2e43
yyxtxt <135936104+yyxtxt@users.noreply.github.com>
2023-06-08 11:51:09 +0800

    Add files via upload

---
+++ bookshop/src/components/home/Homebooks.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="homebooks">
+		<p class="xs">新书上市</p>
+		<div class="xsss">
+			<div v-for=" item in pack" class="c">
+				
+			<a :href="`/book/${item.id}`" class="cc">
+			<img :src="item.imgUrl" />
+			<p>{{item.title}}</p>
+			<span style="font-weight: bold;">
+			￥{{(item.price*item.discount).toFixed(2)}} </span><span>￥{{item.price.toFixed(2)}}</span>
+			
+			</a>
+		</div>
+		</div>
+		
+		
+	</div>
+</template>
+
+<script>
+	export default{
+		props:['pack'],
+		mounted() {
+			console.log(this.pack)
+		}
+	}
+</script>
+
+<style scoped="scoped">
+	.homebooks{
+		width: 100%;
+		height: 440px;
+		background-color: #d9defe;
+	}
+	.xsss{
+		width: 80%;
+		margin: auto;
+		display: flex;
+		justify-content: space-around;
+	}
+	.c{
+		background-color: white;
+		border-radius: 10px;
+		width: 230px;
+		height: 300px;
+		padding-top: 20px;
+	}
+	.cc{
+		text-decoration: none;
+	}
+	.xs{
+		color: white;
+		font-size: 20px;
+		font-weight: bold;
+		line-height: 60px;
+	}
+</style>
---
+++ bookshop/src/router/index.js
@@ -0,0 +1,151 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Loading from '../components/Loading.vue'
+import BooksNew from '../components/BooksNew.vue'
+// import HeaderSeach from '../components/home/HeaderSearch.vue'
+import Headerxs from '../components/Headerxs.vue'
+// import HeaderSeach from '../components/home/HeaderSearch.vue'
+import Books from '../views/Books.vue'
+// import BooksNew from '../components/BooksNew.vue'
+import Header from '../components/Header.vue'
+import login from '../components/Login/login.vue'
+import enroll from '../components/Login/enroll.vue'
+import sj from '../components/Login/sj.vue'
+import checkout from '../views/CheckOut.vue'
+import ShopCart from '../views/ShopCart.vue'
+// import BookDetail from '../views/BookDetail.vue'
+import BookDetail from '../views/BookDetail.vue'
+//@表示的是src文件夹，vue为了更方便读取src的内部文件所设置的相对路径
+import store from '@/store'
+
+Vue.use(VueRouter)
+
+
+const routes = [
+	{
+		path:'/',
+		// name:'htgl',
+		// component:htgl,
+		redirect:'/header/home'
+	},
+	{
+		path:'/header',
+		name:'Header',
+		component:Header,
+		children:[
+			{
+				path:'home',
+				name:'Home',
+				components:{
+					default:Home,
+					header:Header,
+				}
+			},
+			{
+				path:'headerxs',
+				name:'Headerxs',
+				component:Headerxs
+			},
+			{
+				path:'/search',
+				name:'BooksSearch',
+				components:{
+					default:Books,
+					header:Header,
+				}
+			},
+			{
+				path:'/category/:id',
+				name:'BookCategory',
+				components:{
+				   default:Books,
+				   header:Header,
+				},
+			},
+			{
+				path:'/shopcart',
+				name:'ShopCart',
+				components:{
+					default:ShopCart,
+					header:Header
+				}
+			},
+			{
+				   path:'/book/:id',
+				   name:'BookDetail',
+				   components:{
+					   header:Header,
+					  default:BookDetail,
+				   },
+				   meta:{
+				   	title:'书籍详情'
+				   }
+			},
+			
+			{
+				path:'/checkout',
+				name:'checkout',
+				components:{
+					default:checkout,
+					header:Header,
+				}
+			},
+			
+		]
+	},
+	{
+		path:'/login',
+		name:'login',
+		components:{
+			default:login,
+		},
+	},
+	{
+		path:'/enroll',
+		name:'enroll',
+		components:{
+			default:enroll,
+		},
+	},
+	{
+		path:'/sj',
+		name:'sj',
+		components:{
+			default:sj
+		}
+	}
+	
+
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+//全局前置路由
+// router.beforeEach( (to , from , next)=>{
+	
+// 	if( to.matched.some( url => url.meta.isLogin)){
+// 		if( store.state.user.user){
+// 			next()
+// 		}else{
+// 			next({
+// 				path:'/Login'
+// 			})
+// 		}
+// 	}
+// })
+
+//设置路由守卫  全局后置 当程序进度到目标组件之后，执行的一系列操作
+// to参数  目标路由
+
+router.afterEach( (to)=>{
+	document.title = to.meta.title;
+})
+
+
+
+export default router
