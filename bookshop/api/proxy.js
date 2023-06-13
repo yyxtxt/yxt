08122b273f7e67659d8755eab219aa21d33dea7d
yyxtxt <135936104+yyxtxt@users.noreply.github.com>
2023-06-13 10:41:26 +0800

    Update proxy.js

--- bookshop/api/proxy.js
+++ bookshop/api/proxy.js
@@ -6,7 +6,7 @@ module.exports = (req, res) => {
 
   // 代理目标地址
   // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
-  if (req.url.startsWith('/shu')) {
+  if (req.url.startsWith('/api')) {
   // 这里填目标地址
     target = 'http://111.229.37.167'
   }
@@ -17,7 +17,7 @@ module.exports = (req, res) => {
     pathRewrite: {
       // 通过路径重写，去除请求路径中的 `/api`
       // 例如 /api/user/login 将被转发到 http://target/user/login
-      '^/shu/': '/',
+      '^/api/': '/',
     }
   })(req, res)
 }
